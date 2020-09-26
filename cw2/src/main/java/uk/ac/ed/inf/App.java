8d9c907bbfbdcf6a2c4a470ae198a84cdd85b601
Klausstaler <fredrik.klauss@gmx.de>
2020-09-26 15:11:28 +0100

    Fix map class

--- cw2/src/main/java/uk/ac/ed/inf/App.java
+++ cw2/src/main/java/uk/ac/ed/inf/App.java
@@ -14,10 +14,17 @@ public class App
     public static void main( String[] args ) throws IllegalAccessException,
             InstantiationException, InvocationTargetException {
 
-        Point pos = Point.fromLngLat(55.946233, -3.192473);
+        Point pos = Point.fromLngLat(-3.192471, 55.946229);
+        Point pos2 = Point.fromLngLat(-3.1924670934677124,
+                55.94625365809505);
+        Point pos3 = Point.fromLngLat(-3.1924670934677124,
+                55.94625365809505);
         DroneLogger logger = new CombinedLogger(pos, "02-02-2021", ReadingLogger.class,
                 FlightPathLogger.class);
-
+        Map map = Map.getInstance();
+        System.out.println(map.inAllowedArea(pos));
+        System.out.println(map.inAllowedArea(pos2));
+        System.out.println(map.inAllowedArea(pos3));
         // BackendService service = new SensorService(
         //        "http://localhost", "80", "02", "02", "2021");
 
--- cw2/src/main/java/uk/ac/ed/inf/Map.java
+++ cw2/src/main/java/uk/ac/ed/inf/Map.java
@@ -1,20 +1,22 @@
 package uk.ac.ed.inf;
 
 import com.mapbox.geojson.Point;
-import com.mapbox.geojson.Polygon;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LinearRing;
+import org.locationtech.jts.geom.Polygon;
 
+import java.nio.charset.CoderResult;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Map {
 
-    private static final Point northEastBound = Point.fromLngLat(-3.184319, 55.946233);
-    private static final Point southWestBound = Point.fromLngLat(-3.192473, 55.942617);
     private static Map instance = null;
-    private List<Polygon> obstacles = new ArrayList<>();
 
-    private Map() {
-    }
+    private Polygon playArea;
+    private GeometryFactory geometryFactory = new GeometryFactory();
+
 
     public static Map getInstance() {
         if (instance == null) {
@@ -23,63 +25,42 @@ public class Map {
         return Map.instance;
     }
 
-    private boolean inMapBoundaries(Point position) {
-        double curr_latitude = position.latitude();
-        double curr_longitude = position.longitude();
-        boolean longitude_check = Map.northEastBound.longitude() > curr_longitude;
-        longitude_check =
-                longitude_check && (Map.southWestBound.longitude() < curr_longitude);
-        boolean latitude_check = Map.northEastBound.latitude() > curr_latitude;
-        latitude_check = latitude_check && (Map.southWestBound.latitude() < curr_latitude);
-        return latitude_check && longitude_check;
+    private Map() {
+        Coordinate[] boundaries = {new Coordinate(-3.192473, 55.946233),
+                new Coordinate(-3.184319, 55.946233),
+                new Coordinate(-3.184319, 55.942617),
+                new Coordinate(-3.192473, 55.942617),
+                new Coordinate(-3.192473, 55.946233)};
+
+        LinearRing[] holes = this.getObstacles();
+        LinearRing shell = this.geometryFactory.createLinearRing(boundaries);
+
+        this.playArea = this.geometryFactory.createPolygon(shell, holes);
     }
 
     public boolean inAllowedArea(Point position) {
-        boolean inAllowedArea = this.inMapBoundaries(position);
+        org.locationtech.jts.geom.Point point =
+                new GeometryFactory().createPoint(new Coordinate(position.longitude(),
+                        position.latitude()));
 
-        for (Polygon obstacle : this.obstacles) {
-            inAllowedArea = inAllowedArea && this.outsidePolygon(obstacle, position);
-        }
-        return inAllowedArea;
+        return this.inAllowedArea(point);
     }
 
-    private boolean outsidePolygon(Polygon obstacle, Point position) {
-        if (this.outsideBoundingRectangle(obstacle, position)) {
-            return true;
-        }
-        List<Point> vertices = obstacle.coordinates().get(0);
-        int nVertices = vertices.size();
-        int i = 0;
-        int j = nVertices;
-        boolean inside = false;
-        for (i = 0, j = nVertices - 1; i < nVertices; j = i++) {
-            Point vert1 = vertices.get(i);
-            Point vert2 = vertices.get(j);
-        }
+    public boolean inAllowedArea(org.locationtech.jts.geom.Point position) {
+        return this.playArea.intersects(position);
     }
 
-    private boolean outsideBoundingRectangle(Polygon obstacle, Point position) {
-        List<Point> vertices = obstacle.coordinates().get(0);
-        double minLong = vertices.get(0).longitude();
-        double maxLong = vertices.get(0).longitude();
-        double minLat = vertices.get(0).latitude();
-        double maxLat = vertices.get(0).latitude();
-
-        for (int i = 1; i < vertices.size(); i++) {
-            Point vertex = vertices.get(1);
-            minLong = Math.min(minLong, vertex.longitude();
-            maxLong = Math.max(maxLong, vertex.longitude());
-            minLat = Math.min(minLat,vertex.latitude());
-            maxLat = Math.max(maxLat, vertex.latitude());
-        }
-
-        double pLong = position.longitude();
-        double pLat = position.latitude();
-
-        boolean outsideLong = pLong < minLong || pLong > maxLong;
-        boolean outsideLat = pLat < minLat || pLat > maxLat;
-
-        return outsideLat || outsideLong;
+    private LinearRing[] getObstacles() {
+        List<LinearRing> obstacles = new ArrayList<>();
+        Coordinate[] coordinates = {
+                new Coordinate(-3.1923430413007736, 55.94617949887322),
+                new Coordinate(-3.1923524290323257,55.94615359004835),
+                new Coordinate(-3.192284032702446, 55.94614795769279),
+                new Coordinate(-3.192264586687088, 55.94617912338314),
+                new Coordinate(-3.1923430413007736, 55.94617949887322)};
+        obstacles.add(this.geometryFactory.createLinearRing(coordinates));
+        System.out.println(obstacles);
+        return obstacles.toArray(new LinearRing[0]);
     }
 
 
