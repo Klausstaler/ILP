59bd04e454c3d6736aace87b7441b74f71a9d861
Klausstaler <fredrik.klauss@gmx.de>
2020-09-27 17:20:01 +0100

    Upload files

--- cw2/src/main/java/uk/ac/ed/inf/GraphOptimizer.java
+++ cw2/src/main/java/uk/ac/ed/inf/GraphOptimizer.java
@@ -1,4 +1,21 @@
 package uk.ac.ed.inf;
 
 public class GraphOptimizer {
+    private final int numDrones = 1;
+    private int startLocation = 0;
+    private long[][] distanceMatrix;
+
+    public GraphOptimizer(long[][] distanceMatrix, int startLocation) {
+        this.distanceMatrix = distanceMatrix;
+        this.startLocation = startLocation;
+        Routing manager =
+                new RoutingIndexManager(data.distanceMatrix.length, data.vehicleNumber, data.depot);
+        RoutingModel routing = new RoutingModel(manager);
+    }
+
+    public GraphOptimizer(long[][] distanceMatrix) {
+        this.distanceMatrix = distanceMatrix;
+    }
+
+
 }
