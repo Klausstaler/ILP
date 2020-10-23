c0a46b7dc2a1817ec72acb894c3cceb565be6eb8
Klausstaler <fredrik.klauss@gmx.de>
2020-10-23 14:22:30 +0100

    Finish or tools migration

--- cw2/src/main/java/uk/ac/ed/inf/GraphOptimizer.java
+++ cw2/src/main/java/uk/ac/ed/inf/GraphOptimizer.java
@@ -32,12 +32,17 @@ public class GraphOptimizer {
 
         int[] sol = new int[this.distanceMatrix.length+1];
         int idx = 0;
+        String id;
         for(var route : Solutions.bestOf(solutions).getRoutes()) {
+            /*
             var id = route.getStart().getLocation().getId();
             sol[idx++] = Integer.parseInt(id);
+            System.out.println(Arrays.toString(sol));
+             */
             for (var activity : route.getActivities()) {
                 id = activity.getLocation().getId();
                 sol[idx++] = Integer.parseInt(id);
+                System.out.println(Arrays.toString(sol));
             }
             id = route.getEnd().getLocation().getId();
             System.out.println(Arrays.toString(sol));
